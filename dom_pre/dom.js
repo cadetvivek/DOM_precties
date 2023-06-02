45e6eef8768c07a878e9e33d1585aff80c9892f4
vivek <106858243+cadetvivek@users.noreply.github.com>
2023-06-02 16:54:55 +0530

    Add files via upload
    its a dom form

---
+++ dom_pre/dom.js
@@ -0,0 +1,23 @@
+// console.log(document.domain)
+// console.log(document.URL)
+// console.log(document.title)
+// //document.title=123
+// console.log(document.head)
+// console.log(document.doctype)
+// console.log(document.body)
+// console.log(document.all)
+// console.log(document.all[10])
+// console.log(document.forms[0])
+// console.log(document.links)
+// console.log(document.images)
+
+// GETELEMENTBYID
+
+var headTitle = document.getElementById('head-title')
+var header = document.getElementById('main-header')
+
+header.style.borderBottom='solid 5px #000'
+var heading = document.querySelector('.title');
+heading.style.color='green'
+
+
